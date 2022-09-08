7d55f1492cc900b4bb8c6b0931afecdecbd2ae30
maniamartial <martiallmania19@gmail.com>
2022-09-08 16:40:53 +0300

    Simple user API

---
+++ academics/serializers.py
@@ -0,0 +1,14 @@
+from email.headerregistry import Group
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields=['url', 'username', 'email']
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model=Group
+        fields=['url', 'name']
+
